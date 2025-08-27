ae8654c7db413c8c5f135cc861772007f54e2cf5
mdahmed4875 <140884439+mdahmed4875@users.noreply.github.com>
2025-08-27 13:01:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function stringChop(str, size) {
   // your code here
+	let ans=[];
+	let n=str.length;
+	for(let i=0;i<n;i+=size){
+		ans.push(str.substring(i,i+size));
+	}
+	return ans;
 }
 
 // Do not change the code below
